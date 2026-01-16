60bbafda6dbf447485cfe8633c5dacef2e454387
Karen <karenavela@gmail.com>
2026-01-16 13:30:20 -0300

    Atividade prática 01 - questão 04

--- atividadepratica01/questao04.py
+++ atividadepratica01/questao04.py
@@ -5,6 +5,6 @@ Quantidade = 3
 valor_total = Preco_unitario * Quantidade
 print("Total da compra")
 print(f"Produto: {nome_produto}")
-print(f"Prrço unitário: {Preco_unitario}")
+print(f"Prrço unitário: {Preco_unitario:.2f}")
 print(f"Quantidade: {Quantidade}")
-print(f"Valor Total: {valor_total}")
+print(f"Valor Total: {valor_total:.2f}")
