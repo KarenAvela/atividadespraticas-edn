114d6c707c0a7db8596e5bb16781c6f1c8e613c7
Karen <karenavela@gmail.com>
2026-01-20 19:48:14 -0300

    Atividade 02 - Questão 01

---
+++ atividadepratica02/questao01.py
@@ -0,0 +1,9 @@
+valor_reais = float (input("Digite o valor em reais:"))
+cotacao_dolar = 5.20
+cotacao_euro = 6.15
+
+valor_dolar = valor_reais / cotacao_dolar
+
+valor_euro = valor_reais / cotacao_euro
+
+print (f"O valor em dólar é: {valor_dolar} e o valor em euro é: {valor_euro}")
